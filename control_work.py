1d67c36e43eba680e2e6c22717db0f9fe38d26c7
spider65 <raniero.albanesi@gmail.com>
2021-08-04 20:31:19 +0200

    ...

--- control_work.py
+++ control_work.py
@@ -15,18 +15,13 @@ st=5
 if vs.micro_cop.when_pressed:
     vacuum_on(vt,st)
 
-def done():        
+def done():
     while True:
         event, values= gv.win.Read(timeout=0.1)
         value=values
         vt=value['vacuum-time']
         st=value['soldier-time']
         #if event=="start": #questo evento va sostituito con lo when_pressed del coperchio
-        vacuum_on(vt,st)
-else:
-    while True:
-        event, values= gv.win.Read(timeout=0.1)
-        value=values
         wt=value['warm-pump-time']
         if event == 'on-warm-pump':
             print ("riscaldamento pompa ")
