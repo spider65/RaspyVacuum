7f4f412c7a61c5c6a4d6d59f43101450f81cde5e
spider65 <raniero.albanesi@gmail.com>
2021-08-06 10:22:03 +0200

    hhh

--- control_work.py
+++ control_work.py
@@ -36,8 +36,8 @@ def vacuum_on(): #vacuum_time,soldier_time):
     vt1=value['vacuum-time']
     st1=value['soldier-time']
     print(vt1,"vaccum time")"""
-    #vt1=vt
-    #st1=st
+    vt1=vt
+    st1=st
     if vs.vacuum_valve.value == 0 and vs.vacuum_coil.value == 0:
         print("inizio vuoto - accensione pompa")
     else:
