9c65b15390215daa01411203aeab39ce1d99ce5e
spider65 <raniero.albanesi@gmail.com>
2021-09-03 16:13:12 +0200

    commentato codice mock

--- vacuum_status.py
+++ vacuum_status.py
@@ -2,10 +2,10 @@
 
 # configurazione per mockare il gpio
 from gpiozero import Device
-from gpiozero.pins.mock import MockFactory
+#from gpiozero.pins.mock import MockFactory
 if Device.pin_factory is not None:
      Device.pin_factory.reset()
-Device.pin_factory = MockFactory()
+#Device.pin_factory = MockFactory()
 
 from gpiozero import LED, Button
 
