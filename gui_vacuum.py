fd106db2bac5e47e868fd053894ad7ef91a38a3d
spider65 <raniero.albanesi@gmail.com>
2022-11-08 15:02:51 +0100

    test2

--- gui_vacuum.py
+++ gui_vacuum.py
@@ -4,7 +4,7 @@ import time
 from gpiozero import LED
 from signal import pause
 
-sg.theme("Light")
+sg.theme("LightBlue1")
 layout = [
 
             [sg.Text('Vacuum Control by Tiranno', size=(25, 1), justification='center', font=("Helvetica", 15))],
